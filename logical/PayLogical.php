b1df1c2ecd0ced988231831ec8e68a052ca05656
VanJames <746439274@qq.com>
2015-11-04 14:20:48 +0800

    添加支付逻辑和文件锁

---
+++ logical/PayLogical.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * 支付逻辑层
+ * @User fanxu(746439274@qq.com)
+ */
+namespace app\logical;
+
+use app\helpers\Tool;
+use Yii;
+
+/**
+ * @property integer $paymentType
+ * @property float $money
+ * @property integer $orderId
+ **/
+class PayLogical extends BaseLogical
+{
+    /**
+     * @var int
+     */
+    const PAYMENT_TYPE_WEI_XIN = 0;
+
+    /**
+     * @var int
+     */
+    const PAYMENT_TYPE_ALI_PAY= 1;
+
+    /**
+     * 前去支付
+     */
+    public function doPay(){
+        switch( $this->paymentType ){
+            case self::PAYMENT_TYPE_WEI_XIN:
+                break;
+            case self::PAYMENT_TYPE_ALI_PAY:
+                break;
+            default :
+                Tool::throwCustomerException('支付方式错误!');
+        }
+    }
+
+    /**
+     * @param int $paymentType
+     * @return $this
+     */
+    public function setPaymentType( $paymentType ){
+        $this->paymentType = $paymentType;
+        return $this;
+    }
+
+    /**
+     * @param float $money
+     * @return $this
+     */
+    public function setMoney( $money ){
+        $this->money = $money;
+        return $this;
+    }
+
+    /**
+     * @param int $orderId
+     * @return $this
+     */
+    public function setOrderId( $orderId ){
+        $this->orderId = $orderId;
+        return $this;
+    }
+}
